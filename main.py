3fb9de34d6be3490d54a1e0203060c61d2de106e
asphodelus-scribe <84209598+asphodelus-scribe@users.noreply.github.com>
2021-05-14 22:28:52 -0400

    losable version
    Can lose the game with working reset board wipe, player has 3 guesses.
    Added duplicate wrong answer filter but something is broken in the check functions so the first duplicate is counted as wrong again.

--- main.py
+++ main.py
@@ -38,34 +38,55 @@ wrongColor = RED
 COLORS = [RED, YELLOW, GREEN, CYAN, BLUE, PURPLE]
 gamePattern  = [0,3,5,10]
 
+showReset = True
 guessesLeft = 3
+prevWrongAnswers = [None]
 
 # this will be called when button events are received
-def blink(event):
-    # turn the LED on when a rising edge is detected
-    if event.edge == NeoTrellis.EDGE_RISING:
-        if button.value:
-            # a button was pushed, now check if it is in the pattern 
-            if event.number in gamePattern: 
-                # button was correct, set to appropriate color
-                trellis.pixels[event.number] = rightColor
-            else:
-                # button was incorrect, set to appropriate color
-                trellis.pixels[event.number] = wrongColor 
-            
-        # otherwise show reset animation
-        else:
-            for j in range(16):
+
+def reset():
+    global guessesLeft
+    global prevWrongAnswers
+    for j in range(16):
                 trellis.pixels[j] = animColor
                 time.sleep(.05)
                 trellis.pixels[j] = OFF
                 time.sleep(.05)
+                showReset = False
+    guessesLeft = 3
+    prevWrongAnswers.clear()
+
+def blink(event):
+    global guessesLeft
+    global prevWrongAnswers
+    # turn the LED on when a rising edge is detected
+    if event.edge == NeoTrellis.EDGE_RISING:
+        print("rem guess",guessesLeft)
+        print("event number",event.number)
+        print("prev wrong",prevWrongAnswers)
+        print("cur button is in prevWrong",event.number not in prevWrongAnswers)
+        if button.value:
+            # a button was pushed, now check if it is in the pattern
+            if event.number in gamePattern:
+                # button was correct, set to appropriate color
+                trellis.pixels[event.number] = rightColor
+            if (event.number in prevWrongAnswers and prevWrongAnswers[0] == None):
+                # wrong answer already input, do nothing
+                pass
+            else:
+               # button was incorrect, set to appropriate color
+                trellis.pixels[event.number] = wrongColor
+                prevWrongAnswers.append(event.number)
+                guessesLeft -= 1
+                if guessesLeft == 0:
+                    reset()
+                    #showReset = True
 
     # do falling edge (todo)
     elif event.edge == NeoTrellis.EDGE_FALLING:
         #trellis.pixels[event.number] = random.choice([RED, YELLOW, GREEN, CYAN, BLUE, PURPLE])
         pass
-        
+
 
 for i in range(16):
     # activate rising edge events on all keys
