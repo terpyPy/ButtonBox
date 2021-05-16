f03937f40b6893cf98510963128f0d434cded02e
asphodelus-scribe <84209598+asphodelus-scribe@users.noreply.github.com>
2021-05-16 17:47:55 -0400

    Timing fix
    fixed possible desync error caused by timing difference in callback adding delay and trellis sync delay (was 0.05, should be 0.02)

--- first_automaton.py
+++ first_automaton.py
@@ -1,7 +1,7 @@
 #Authors Hunter Hannula, Cameron Kerley
 
 import time
-import random # possibly remove, if we use random we should write a seprate module for those functions
+from random import randrange # possibly remove, if we use random we should write a seprate module for those functions
 from adafruit_seesaw.neopixel import NeoPixel
 import board
 from board import SCL, SDA
@@ -19,7 +19,7 @@ button_pin = board.D6
 
 button = digitalio.DigitalInOut(button_pin)
 button.direction = digitalio.Direction.INPUT
-button.pull = digitalio.Pull.UP
+button.pull = digitalio.Pull.DOWN
 
 # some color definitions
 OFF = (0, 0, 0)
@@ -33,27 +33,28 @@ WHITE = (127, 127, 127)
 COLORS = [RED, YELLOW, GREEN, CYAN, BLUE, PURPLE, OFF]
 
 boardArray = [OFF]*16
-boardArray[0] = PURPLE
 
-def redrawBoard():
-    global COLORS
+
+def redrawBoard(colorArray):
+    
     #global boardArray
-    off = COLORS[6]
+    off = colorArray[6]
+    trellis.pixels[0] = colorArray[randrange(0,5)]
+    time.sleep(.05)
     for i in range(len(boardArray)):
         print("I:",i)
         if (boardArray[i] == off) and (i < 16) :
-            trellis.pixels[i] = COLORS[random.randrange(0,5)]
-        time.sleep(.03)
-
-
+            trellis.pixels[i] = colorArray[randrange(0,5)]
+            time.sleep(.05)
 def blink(event):
     # turn the LED on when a rising edge is detected
     if event.edge == NeoTrellis.EDGE_RISING:
         if button.value:
             print("try redraw board")
-            redrawBoard()
+            
     elif event.edge == NeoTrellis.EDGE_FALLING:
         print("falling")
+        redrawBoard(COLORS)
         pass
 
 for i in range(16):
@@ -63,9 +64,15 @@ for i in range(16):
     trellis.activate_key(i, NeoTrellis.EDGE_FALLING)
     # set all keys to trigger the blink callback
     trellis.callbacks[i] = blink
-
+    time.sleep(.02)
+    trellis.pixels[i] = (0, 0, 0)
+    time.sleep(.02)
 while True:
     # call the sync function call any triggered callbacks
     trellis.sync()
+    # try:
+    #     trellis.sync()
+    # except OSError:
+    #     print("THIS IS PROBLEM")
     # the trellis can only be read every 17 millisecons or so
     time.sleep(.02)
